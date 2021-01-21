7be8ea4303e776c00463b9d270f838ad19a1e111
NateC3 <chengmnathan@gmail.com>
2021-01-21 13:34:15 -0500

    post repo

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,5 +1,8 @@
 package com.pluralsight.blog.data;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
+
 import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,21 +12,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-@Component
-public class PostRepository {
-    public List<Post> findAll() {
-        return new ArrayList<>();
-    }
+public interface PostRepository extends JpaRepository<Post, Long> {
+    @RestResource(rel="contains-title", path="containsTitle")
+    public List<Post> findByTitleContaining(String title);
 
-    public List<Post> saveAll(List<Post> posts) {
-        return new ArrayList<>();
-    }
-
-    public Post save(Post post) {
-        return null;
-    }
-
-    public Optional<Post> findById(Long id) {
-        return null;
-    }
 }
+
+//@Component
+//public class PostRepository {
+//    public List<Post> findAll() {
+//        return new ArrayList<>();
+//    }
+//
+//    public List<Post> saveAll(List<Post> posts) {
+//        return new ArrayList<>();
+//    }
+//
+//    public Post save(Post post) {
+//        return null;
+//    }
+//
+//    public Optional<Post> findById(Long id) {
+//        return null;
+//    }
+// }
